366877d5af859dcdbf7665d484c798a190e3e2af
jack gaarkeuken <jack.gaarkeuken@gmail.com>
2020-03-05 18:37:34 -0500

    added firebaseDB

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase.app";
 import "firebase/firestore";
 
 const firebaseConfig = firebase.initializaApp({
-  apiKey: "",
-  authDomain: "",
-  databseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messageSender: "",
-  apiId: ""
+  apiKey: "AIzaSyBMGdVFLo20ZpyZgv7PFTmq3FTZofqIPT8",
+  authDomain: "my-todolist-6d0fd.firebaseapp.com",
+  databaseURL: "https://my-todolist-6d0fd.firebaseio.com",
+  projectId: "my-todolist-6d0fd",
+  storageBucket: "my-todolist-6d0fd.appspot.com",
+  messagingSenderId: "436608999888",
+  appId: "1:436608999888:web:4db14868c13395fb6719d5"
 });
 
 export { firebaseConfig as firebase };

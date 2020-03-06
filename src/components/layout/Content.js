137ef22360d19bc221ab005d25693d8babfd7ca8
jack gaarkeuken <jack.gaarkeuken@gmail.com>
2020-03-05 19:11:44 -0500

    added hooks for tasks

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Sidebar } from "./Sidebar";
-//import { Tasks } from "../Tasks";
+import { Tasks } from "../Tasks";
 
 export const Content = () => (
   <section className="content">
     <Sidebar />
+    <Tasks />
   </section>
 );
